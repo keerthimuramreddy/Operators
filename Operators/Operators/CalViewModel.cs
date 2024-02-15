58011522559449e32f3678cceba6d7f853506380
kmuramreddy <kmuramreddy@US-GMD7XM3>
2024-02-15 12:23:16 +0530

    Add project files.

---
+++ Operators/Operators/CalViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Operators
+{
+    public class CalViewModel : INotifyPropertyChanged
+    {
+        public CalViewModel()
+        {
+            AddCommand = new Command(() =>
+            {
+                Result = Add().ToString();
+
+            });
+            SubCommand = new Command(() =>
+            {
+                Result = Subtract().ToString();
+
+            });
+            MulCommand = new Command(() =>
+            {
+                Result = Multiply().ToString();
+
+            });
+            DivCommand = new Command(() =>
+            {
+                Result = Divide().ToString();
+
+            });
+
+        }
+        private int _num1;
+        private int _num2;
+        private string _result;
+
+        public int Num1
+        {
+            get => _num1;
+            set
+            {
+                _num1 = value;
+                OnPropertyChanged(nameof(Num1));
+            }
+        }
+
+        public int Num2
+        {
+            get => _num2;
+            set
+            {
+                _num2 = value;
+                OnPropertyChanged(nameof(Num2));
+            }
+        }
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public int Add()
+        {
+            return Num1 + Num2;
+        }
+
+        public int Subtract()
+        {
+            return Num1 - Num2;
+        }
+
+        public int Multiply()
+        {
+            return Num1 * Num2;
+        }
+
+        public double Divide()
+        {
+            if (Num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return (double)Num1 / Num2;
+        }
+        public Command AddCommand { get; }
+        public Command SubCommand { get; }
+        public Command MulCommand { get; }
+        public Command DivCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Operators/Operators/OperatorsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Operators
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OperatorsPage : ContentPage
+    {
+        public OperatorsPage()
+        {
+            InitializeComponent();
+        }
+   //     private void Button_Clicked(object sender, EventArgs e)
+   //     {
+            //int num1 = Convert.ToInt32(Num1.Text);
+            //int num2 = Convert.ToInt32(Num2.Text);
+            //int c = num1 + num2;
+            //result.Text = c.ToString();
+       // }
+    }
+}
